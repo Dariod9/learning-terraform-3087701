e76915a6c72aed482178a8808d57c67a2ece4eee
Dário Matos <45235143+Dariod9@users.noreply.github.com>
2023-06-16 16:47:25 +0100

    Module as SG

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog_sg.id]
+  vpc_security_group_ids = [module.blog_sec_g.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -32,6 +32,20 @@ resource "aws_instance" "blog" {
 variable "AWS_SECRET_ACCESS_KEY"{
     type = string
     description = "aws key"
+}
+
+module "blog_sec_g" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id      = data.aws_vpc.default.id
+  
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog_sg"{
