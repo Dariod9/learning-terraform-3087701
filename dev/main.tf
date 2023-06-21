8d50f72fd1406548b2e98ad9aafadf86716fcc04
Dário Matos <45235143+Dariod9@users.noreply.github.com>
2023-06-21 14:42:07 +0100

    back to basic

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,9 @@
+variable "AWS_SECRET_ACCESS_KEY"{
+    type = string
+    description = "aws key"
+}
+
 module "dev" {
     source = "../modules/blog"
+    AWS_SECRET_ACCESS_KEY = "E6Vy4yi9TcER2OXV1wjtEm05Ovp2tln6AoQxRkEn"
 }
