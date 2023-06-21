d05ff59745be669ac55ce59d22d5822d507a69af
Dário Matos <45235143+Dariod9@users.noreply.github.com>
2023-06-21 11:08:54 +0100

    one more fix

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,8 @@
-module "dev" {
-    source = "../modules/blog"
-}
-
 variable "AWS_SECRET_ACCESS_KEY"{
     type = string
     description = "aws key"
+}
+
+module "dev" {
+    source = "../modules/blog"
 }
